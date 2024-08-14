70fe2da4597b3b58c9b64c7017ec8f6d0d1f6105
zft9xgy <zft9xgy@icloud.com>
2024-08-14 12:52:28 +0200

    Add: whitenoise

--- todux/wsgi.py
+++ todux/wsgi.py
@@ -1,14 +1,5 @@
-"""
-WSGI config for todux project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todux.settings')
